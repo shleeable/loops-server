3b52c79e85c8a0f83cb9f9488d714a1027a2b1cf
Daniel Supernault <danielsupernault@gmail.com>
2025-06-26 00:13:13 -0600

    Create ApiHelpers.php

---
+++ app/Http/Controllers/Api/Traits/ApiHelpers.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Api\Traits;
+
+use Stevebauman\Purify\Facades\Purify;
+
+trait ApiHelpers
+{
+    public function data($data, $addl = false, $code = 200, $headers = [])
+    {
+        $res = compact('data');
+        if ($addl && is_array($addl)) {
+            array_merge($res, $addl);
+        }
+
+        return response()->json($res, $code, $headers);
+    }
+
+    public function error($msg, $code = 403, $headers = [])
+    {
+        return response()->json([
+            'data' => [],
+            'error' => [
+                'code' => $code,
+                'message' => $msg,
+            ],
+        ], $code, $headers);
+    }
+
+    public static function purify($text)
+    {
+        return Purify::clean($text);
+    }
+
+    public function success()
+    {
+        return response()->json([
+            'data' => [],
+            'error' => [
+                'code' => 'ok',
+                'message' => '',
+            ],
+        ]);
+    }
+}
