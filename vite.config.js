a333f3e766cb02bd2d41e47dfd802a858f5b649a
Daniel Supernault <danielsupernault@gmail.com>
2025-06-26 00:06:12 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from 'path'
+import { ensureDir, copy } from 'fs-extra'
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
                 'resources/sass/app.scss',
+                'resources/sass/landing.scss',
+                'resources/sass/dashboard.scss',
+                'resources/sass/next.css',
                 'resources/js/app.js',
             ],
             refresh: true,
@@ -22,7 +28,19 @@ export default defineConfig({
     ],
     resolve: {
         alias: {
+            '~': '/resources/js',
+            '@': '/resources/js',
             vue: 'vue/dist/vue.esm-bundler.js',
         },
     },
+    optimizeDeps: {
+        exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
+    },
+    server: {
+        cors: true,
+        headers: {
+            'Cross-Origin-Opener-Policy': 'same-origin',
+            'Cross-Origin-Embedder-Policy': 'require-corp',
+        }
+    }
 });
