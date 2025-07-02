32ab61eb5ad1f1cec2902628fb79964b856db4fa
Daniel Supernault <danielsupernault@gmail.com>
2025-07-01 22:02:56 -0600

    Add Comment Resource

---
+++ app/Http/Resources/CommentResource.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Services\AccountService;
+use App\Services\LikeService;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CommentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $pid = $request->user() ? $request->user()->profile_id : false;
+        $res = [
+            'id' => (string) $this->id,
+            'v_id' => (string) $this->video_id,
+            'account' => AccountService::compact($this->profile_id),
+            'caption' => $this->caption,
+            'likes' => $this->likes ?? 0,
+            'replies' => $this->replies ?? 0,
+            'children' => [],
+            'liked' => $pid ? LikeService::hasCommentLike($this->id, $pid) : false,
+            'url' => $this->shareUrl(),
+            'is_owner' => $pid ? (string) $this->profile_id === (string) $pid : false,
+            'created_at' => $this->created_at->format('c'),
+        ];
+
+        return $res;
+    }
+}
