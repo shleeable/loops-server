9dc9d7fad6e0364f5b24cf46cc8b9e0cfadca7e7
Daniel Supernault <danielsupernault@gmail.com>
2025-07-01 21:31:29 -0600

    Add Notification Resource

---
+++ app/Http/Resources/NotificationResource.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Notification;
+use App\Services\AccountService;
+use App\Services\VideoService;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class NotificationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        switch ($this->type) {
+            case Notification::VIDEO_LIKE:
+                return $this->newVideoLike();
+                break;
+
+            case Notification::NEW_FOLLOWER:
+                return $this->newFollower();
+                break;
+
+            case Notification::NEW_VIDCOMMENT:
+                return $this->newVideoComment();
+                break;
+
+            default:
+                return [
+                    'id' => (string) $this->id,
+                    'type' => 'internal',
+                    'created_at' => $this->created_at,
+                ];
+                break;
+        }
+    }
+
+    protected function newVideoLike()
+    {
+        $video = VideoService::getMediaData($this->video_id);
+        $thumb = data_get($video, 'media.thumbnail', null);
+
+        return [
+            'id' => (string) $this->id,
+            'type' => 'video.like',
+            'video_id' => (string) $this->video_id,
+            'video_thumbnail' => $thumb,
+            'actor' => AccountService::compact($this->profile_id),
+            'created_at' => $this->created_at,
+        ];
+    }
+
+    protected function newFollower()
+    {
+        return [
+            'id' => (string) $this->id,
+            'type' => 'new_follower',
+            'actor' => AccountService::compact($this->profile_id),
+            'created_at' => $this->created_at,
+        ];
+    }
+
+    protected function newVideoComment()
+    {
+        $video = VideoService::getMediaData($this->video_id);
+        $thumb = data_get($video, 'media.thumbnail', null);
+
+        return [
+            'id' => (string) $this->id,
+            'type' => 'video.comment',
+            'actor' => AccountService::compact($this->profile_id),
+            'video_id' => (string) $this->video_id,
+            'video_thumbnail' => $thumb,
+            'created_at' => $this->created_at,
+        ];
+    }
+}
