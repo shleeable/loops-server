5994e47d361bc18f72ff25a99314fccaf2e90a88
Daniel Supernault <danielsupernault@gmail.com>
2025-07-06 03:33:24 -0600

    Add UserObserver

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Profile;
+use App\Models\User;
+use Illuminate\Contracts\Events\ShouldHandleEventsAfterCommit;
+
+class UserObserver implements ShouldHandleEventsAfterCommit
+{
+    /**
+     * Handle the User "created" event.
+     */
+    public function created(User $user): void
+    {
+        $profile = Profile::updateOrCreate([
+            'user_id' => $user->id,
+        ], [
+            'username' => $user->username,
+            'name' => $user->name,
+        ]);
+        $user->profile_id = $profile->id;
+        $user->save();
+    }
+
+    /**
+     * Handle the User "updated" event.
+     */
+    public function updated(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "deleted" event.
+     */
+    public function deleted(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "restored" event.
+     */
+    public function restored(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "force deleted" event.
+     */
+    public function forceDeleted(User $user): void
+    {
+        //
+    }
+}
