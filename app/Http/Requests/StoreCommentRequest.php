dbfc6e4d5291e48065723dcf5c59356a88ac2fbc
Daniel Supernault <danielsupernault@gmail.com>
2025-07-12 01:06:01 -0600

    Add StoreComment form request

---
+++ app/Http/Requests/StoreCommentRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Comment;
+use App\Models\Video;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCommentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        if (! $this->user() || ! $this->user()->can_comment) {
+            return false;
+        }
+        $video = Video::published()->canComment()->find($this->route('id'));
+
+        return $video && $this->user()->can('create', [Comment::class, $video]);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'parent_id' => 'sometimes|integer|exists:comments,id',
+            'comment' => 'required|string|min:2|max:500',
+        ];
+    }
+}
