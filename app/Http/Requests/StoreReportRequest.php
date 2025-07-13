fae1aec617ba0c46db6190a1ca9cb8193acdfab6
Daniel Supernault <danielsupernault@gmail.com>
2025-07-12 21:28:31 -0600

    Add StoreReport form request

---
+++ app/Http/Requests/StoreReportRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreReportRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        if(!$this->user()) {
+            return false;
+        }
+
+        if($this->user()->trust_level < 5) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'type' => 'required|in:video,profile,comment,reply',
+            'id' => 'required|string',
+            'key' => 'required|integer|min:1010|max:1026',
+            'comment' => 'sometimes|nullable|string|min:1|max:500'
+        ];
+    }
+}
