ff0f522899b4a14e3d295886b754415de516c88a
Daniel Supernault <danielsupernault@gmail.com>
2025-07-16 05:28:24 -0600

    Add SearchRequest

---
+++ app/Http/Requests/SearchRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SearchRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        if (! $this->user()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'q' => [
+                'required',
+                'string',
+                'min:1',
+                'max:50',
+                'regex:/^[@]?[a-zA-Z0-9._-]+$/',
+            ],
+        ];
+    }
+}
