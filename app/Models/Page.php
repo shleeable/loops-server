838f880b831ab2d09a2739411478c9641625390c
Daniel Supernault <danielsupernault@gmail.com>
2025-07-19 14:04:27 -0600

    Add Page model

---
+++ app/Models/Page.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Page extends Model
+{
+    protected $fillable = [
+        'title',
+        'slug',
+        'content',
+        'status',
+        'meta',
+        'location',
+        'created_at',
+    ];
+
+    protected $casts = [
+        'meta' => 'array',
+        'system_page' => 'boolean',
+    ];
+
+    public function scopePublished($query)
+    {
+        return $query->where('status', 'published');
+    }
+
+    public function scopeDraft($query)
+    {
+        return $query->where('status', 'draft');
+    }
+}
