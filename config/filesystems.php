8d6acce193e098146bb114f8ab9c13dba56cb279
Daniel Supernault <danielsupernault@gmail.com>
2025-07-20 16:12:45 -0600

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -29,6 +29,12 @@ return [
     */
 
     'disks' => [
+        'app' => [
+            'driver' => 'local',
+            'root' => storage_path('app'),
+            'serve' => true,
+            'throw' => false,
+        ],
 
         'local' => [
             'driver' => 'local',
