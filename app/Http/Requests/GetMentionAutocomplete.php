a91f71c6df56020477f676bf07ea17b0f625876c
Daniel Supernault <danielsupernault@gmail.com>
2025-07-20 02:55:33 -0600

    Add GetMentionAutocomplete form request

---
+++ app/Http/Requests/GetMentionAutocomplete.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class GetMentionAutocomplete extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return $this->user() && $this->user()->can_upload;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'q' => 'required|string|min:1|max:50',
+            'limit' => 'sometimes|int|min:1|max:10',
+        ];
+    }
+}
