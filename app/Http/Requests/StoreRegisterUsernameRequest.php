f32419dc613d1a806cc83cc53dc08390429fe021
Daniel Supernault <danielsupernault@gmail.com>
2025-07-25 05:21:26 -0600

    Add StoreRegisterUsername form request

---
+++ app/Http/Requests/StoreRegisterUsernameRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\AdminSetting;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreRegisterUsernameRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        if ($this->user()) {
+            throw new AuthorizationException('You are already logged in, you must logout before registering a new account.');
+        }
+
+        if (config('mail.default') == 'log') {
+            throw new AuthorizationException('Mail service not configured, please contact support for assistance.');
+        }
+
+        if (AdminSetting::where('key', 'general.openRegistration')->whereRaw("JSON_EXTRACT(value, '$') = false")->exists()) {
+            throw new AuthorizationException('Registration is disabled, please try again later.');
+        }
+
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'username' => [
+                'required',
+                'alpha_dash',
+                'min:2',
+                'max:30',
+                'unique:users,username',
+                Rule::notIn(['admin', 'dansup', 'support', 'loops', 'official', 'team', 'teamLoops', 'new', 'discover', 'explore']),
+            ],
+            'password' => 'required|min:8',
+            'password_confirmation' => 'required|confirmed:password',
+        ];
+    }
+}
