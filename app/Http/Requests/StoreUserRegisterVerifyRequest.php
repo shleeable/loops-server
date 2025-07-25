e4257664f1f315d20ccbb7fd5ff3b0066f64423c
Daniel Supernault <danielsupernault@gmail.com>
2025-07-25 05:22:02 -0600

    Add StoreUserRegisterVerifyRequest form request

---
+++ app/Http/Requests/StoreUserRegisterVerifyRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\AdminSetting;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUserRegisterVerifyRequest extends FormRequest
+{
+    public $denyMessage;
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        if ($this->user()) {
+            throw new AuthorizationException('You are already logged in, you must logout before registering a new account.');
+        }
+
+        if (config('mail.default') == 'log') {
+            throw new AuthorizationException('Mail service not configured, please contact support for assistance.');
+        }
+
+        if (AdminSetting::where('key', 'general.openRegistration')->whereRaw("JSON_EXTRACT(value, '$') = false")->exists()) {
+            throw new AuthorizationException('Registration is disabled, please try again later.');
+        }
+
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => [
+                'required',
+                'email:rfc,dns,spoof,strict',
+                'unique:user_register_verifies,email',
+                'unique:users,email',
+            ],
+        ];
+    }
+}
