c43fc10b4006f1cf34e187da650cee2cdf8d048b
Daniel Supernault <danielsupernault@gmail.com>
2025-07-27 04:40:44 -0600

    Update web routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/{vue?}', 'welcome')->where('vue', '.*');
+
+Auth::routes([
+    'login' => true,
+    'logout' => true,
+    'register' => false,
+    'reset' => true,
+    'confirm' => false,
+    'verify' => false,
+]);
