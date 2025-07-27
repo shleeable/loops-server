dea67b6f0b85b8a4534fde3b1299e89abafbfd4e
Daniel Supernault <danielsupernault@gmail.com>
2025-07-27 04:03:42 -0600

    Update ForgotPasswordController

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Rules\HCaptchaRule;
+use App\Rules\TurnstileRule;
+use App\Services\CaptchaService;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\Request;
 
 class ForgotPasswordController extends Controller
 {
@@ -19,4 +23,37 @@ class ForgotPasswordController extends Controller
     */
 
     use SendsPasswordResetEmails;
+
+    /**
+     * Validate the email for the given request.
+     *
+     * @return void
+     */
+    protected function validateEmail(Request $request)
+    {
+        $hasCaptcha = config('captcha.enabled');
+
+        $rules = [
+            'email' => 'required|email:rfc,dns,spoof,strict',
+        ];
+
+        if ($hasCaptcha) {
+            $rules['captcha_type'] = ['required', 'in:turnstile,hcaptcha'];
+
+            if (config('captcha.driver') === 'turnstile') {
+                $rules['captcha_token'] = [
+                    'required',
+                    'string',
+                    new TurnstileRule(new CaptchaService),
+                ];
+            } elseif (config('captcha.driver') === 'hcaptcha') {
+                $rules['captcha_token'] = [
+                    'required',
+                    'string',
+                    new HCaptchaRule(new CaptchaService),
+                ];
+            }
+        }
+        $request->validate($rules);
+    }
 }
