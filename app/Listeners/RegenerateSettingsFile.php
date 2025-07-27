5c0120e3af6e7ddf9c3f892fdccf0224fbe304b3
Daniel Supernault <danielsupernault@gmail.com>
2025-07-26 21:31:37 -0600

    Update RegenerateSettingsFile settings

--- app/Listeners/RegenerateSettingsFile.php
+++ app/Listeners/RegenerateSettingsFile.php
@@ -8,8 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 
 class RegenerateSettingsFile implements ShouldQueue
 {
-    public function __construct()
-    {}
+    public function __construct() {}
 
     public function handle(SettingsUpdated $event)
     {
