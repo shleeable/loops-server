843a13e30e5e3b13e4485e2d4eead1e7f9fe68bd
Daniel Supernault <danielsupernault@gmail.com>
2025-07-27 01:03:51 -0600

    Add ProfileObserver

---
+++ app/Observers/ProfileObserver.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Profile;
+use App\Services\AccountService;
+use Illuminate\Contracts\Events\ShouldHandleEventsAfterCommit;
+
+class ProfileObserver implements ShouldHandleEventsAfterCommit
+{
+    /**
+     * Handle the Profile "created" event.
+     */
+    public function created(Profile $profile): void
+    {
+        AccountService::del($profile->id);
+    }
+
+    /**
+     * Handle the Profile "updated" event.
+     */
+    public function updated(Profile $profile): void
+    {
+        AccountService::del($profile->id);
+    }
+
+    /**
+     * Handle the Profile "deleted" event.
+     */
+    public function deleted(Profile $profile): void
+    {
+        AccountService::del($profile->id);
+    }
+
+    /**
+     * Handle the Profile "restored" event.
+     */
+    public function restored(Profile $profile): void
+    {
+        AccountService::del($profile->id);
+    }
+
+    /**
+     * Handle the Profile "force deleted" event.
+     */
+    public function forceDeleted(Profile $profile): void
+    {
+        AccountService::del($profile->id);
+    }
+}
