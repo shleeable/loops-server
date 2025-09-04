063718d5cae97c81a7072027af715c3896e00a0f
Daniel Supernault <danielsupernault@gmail.com>
2025-09-04 02:10:07 -0600

    Create posts.vue

---
+++ resources/js/pages/studio/posts.vue
@@ -0,0 +1,9 @@
+<template>
+    <StudioLayout>
+        <StudioPosts api-base="/api/v1/studio/posts" />
+    </StudioLayout>
+</template>
+
+<script setup>
+import StudioPosts from "@/components/Studio/StudioPosts.vue";
+</script>
