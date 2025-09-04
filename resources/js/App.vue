99e8ce345cef4dc1b9e129df168a69cbcc1e6870
Daniel Supernault <danielsupernault@gmail.com>
2025-09-04 00:46:14 -0600

    Add bootstrap/app js

---
+++ resources/js/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <router-view />
+    <AuthModal v-if="authStore.isOpen" :mode="authStore.authMode" />
+</template>
+
+<script setup>
+import { onMounted, watch, inject } from "vue";
+import { useAuthStore } from "@/stores/auth";
+import AuthModal from "@/components/AuthModal.vue";
+
+const authStore = useAuthStore();
+
+onMounted(async () => {
+    if (
+        localStorage.theme === "dark" ||
+        (!("theme" in localStorage) &&
+            window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+        document.documentElement.classList.add("dark");
+    } else {
+        document.documentElement.classList.remove("dark");
+    }
+    try {
+        await authStore.hasSessionExpired();
+    } catch (error) {
+        console.error("Error in App setup:", error);
+    }
+});
+</script>
---
+++ resources/js/app.js
@@ -0,0 +1,55 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+import "./bootstrap";
+import { createApp } from "vue";
+import { createMemoryHistory, createRouter } from "vue-router";
+import { createPinia } from "pinia";
+import axiosPlugin from "./plugins/axios";
+import { VueQueryPlugin } from "@tanstack/vue-query";
+import AlertModalPlugin from "@/composables/useAlertModal.js";
+import storePlugin from "./plugins/stores";
+import App from "./App.vue";
+import router from "./routes/index";
+import "@mdi/font/css/materialdesignicons.min.css";
+import "boxicons/css/boxicons.min.css";
+import "remixicon/fonts/remixicon.css";
+
+const app = createApp(App);
+const pinia = createPinia();
+
+Object.entries(import.meta.glob("./**/*.vue", { eager: true })).forEach(
+    ([path, definition]) => {
+        app.component(
+            path
+                .split("/")
+                .pop()
+                .replace(/\.\w+$/, ""),
+            definition.default,
+        );
+    },
+);
+
+app.config.globalProperties.$appConfig = window.appConfig;
+app.provide("appConfig", window.appConfig);
+app.provide("appCaptcha", window.appCaptcha);
+
+app.use(pinia)
+    .use(axiosPlugin)
+    .use(router)
+    .use(storePlugin)
+    .use(AlertModalPlugin)
+    .use(VueQueryPlugin, {
+        queryClientConfig: {
+            defaultOptions: {
+                queries: {
+                    staleTime: 1000 * 60 * 5,
+                    refetchOnWindowFocus: false,
+                },
+            },
+        },
+    })
+    .mount("#app");
