a7b2cce48a58c1b1d7388bf7db360b66552f575c
Daniel Supernault <danielsupernault@gmail.com>
2025-09-04 02:10:03 -0600

    Create index.vue

---
+++ resources/js/pages/studio/index.vue
@@ -0,0 +1,102 @@
+<template>
+    <StudioLayout>
+        <div class="min-h-screen bg-gray-50 dark:bg-gray-900">
+            <div class="max-w-7xl mx-auto px-6 py-8">
+                <div
+                    class="border border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 p-4 rounded-lg"
+                >
+                    <div class="flex items-center gap-3">
+                        <img
+                            :src="user.avatar"
+                            class="w-20 h-20 rounded-full border border-gray-200 dark:border-gray-700 p-1"
+                        />
+                        <div class="flex flex-col gap-1">
+                            <div class="text-lg font-bold dark:text-white">
+                                {{ user.username }}
+                            </div>
+
+                            <div class="flex gap-2">
+                                <div
+                                    class="text-sm text-gray-500 dark:text-gray-400"
+                                >
+                                    <span
+                                        class="font-bold text-black dark:text-white"
+                                        >{{
+                                            formatCount(user.post_count)
+                                        }}</span
+                                    >
+                                    Posts
+                                </div>
+                                <div
+                                    class="text-sm text-gray-500 dark:text-gray-400"
+                                >
+                                    ·
+                                </div>
+                                <div
+                                    class="text-sm text-gray-500 dark:text-gray-400"
+                                >
+                                    <span
+                                        class="font-bold text-black dark:text-white"
+                                        >{{
+                                            formatCount(user.likes_count)
+                                        }}</span
+                                    >
+                                    Likes
+                                </div>
+                                <div
+                                    class="text-sm text-gray-500 dark:text-gray-400"
+                                >
+                                    ·
+                                </div>
+                                <div
+                                    class="text-sm text-gray-500 dark:text-gray-400"
+                                >
+                                    <span
+                                        class="font-bold text-black dark:text-white"
+                                        >{{
+                                            formatCount(user.follower_count)
+                                        }}</span
+                                    >
+                                    Followers
+                                </div>
+                                <div
+                                    class="text-sm text-gray-500 dark:text-gray-400"
+                                >
+                                    ·
+                                </div>
+                                <div
+                                    class="text-sm text-gray-500 dark:text-gray-400"
+                                >
+                                    <span
+                                        class="font-bold text-black dark:text-white"
+                                        >{{
+                                            formatCount(user.following_count)
+                                        }}</span
+                                    >
+                                    Following
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </StudioLayout>
+</template>
+
+<script setup>
+import {
+    ref,
+    reactive,
+    computed,
+    nextTick,
+    watch,
+    inject,
+    onMounted,
+} from "vue";
+import { useUtils } from "@/composables/useUtils";
+const authStore = inject("authStore");
+const { formatCount, formatDate } = useUtils();
+
+const user = computed(() => authStore.getUser);
+</script>
