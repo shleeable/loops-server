847deef21d85d331276499a530e102e9f54d3488
Daniel Supernault <danielsupernault@gmail.com>
2025-09-04 01:26:32 -0600

    Create Spinner.vue

---
+++ resources/js/components/Spinner.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="flex items-center justify-center">
+        <div
+            class="h-10 w-10 rounded-full border-4 border-gray-200 border-t-[#F02C56] animate-spin"
+        ></div>
+    </div>
+</template>
