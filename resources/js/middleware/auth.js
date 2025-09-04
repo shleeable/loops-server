10ea6415ac2d1a1dd4e80d9485e86237f35b8ee2
Daniel Supernault <danielsupernault@gmail.com>
2025-09-04 01:35:46 -0600

    Create auth.js

---
+++ resources/js/middleware/auth.js
@@ -0,0 +1,13 @@
+import { useAuthStore } from "~/stores/auth";
+
+export function authMiddleware(to, from, next) {
+    const authStore = useAuthStore();
+
+    if (to.meta.requiresAuth && !authStore.isAuthenticated) {
+        next({ name: "login", query: { redirect: to.fullPath } });
+    } else if (to.meta.guestOnly && authStore.isAuthenticated) {
+        next({ name: "dashboard" });
+    } else {
+        next();
+    }
+}
