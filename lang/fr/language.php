5472887daa72b4a7be6623579727d814950e0f1c
Daniel Supernault <danielsupernault@gmail.com>
2025-09-06 01:17:52 -0600

    Update language.php
    Fix i18n key

--- lang/fr/language.php
+++ lang/fr/language.php
@@ -5,7 +5,7 @@ return [
         'title' => 'Sélectionner la langue',
         'description' => 'Choisissez votre langue préférée. Cela changera la langue de toute l’application.',
         'select' => 'Langue :',
-        'apply' => 'Appliquer les changements',
+        'applyChanges' => 'Appliquer les changements',
         'current' => 'Langue actuelle :',
         'note' => 'Votre préférence linguistique sera enregistrée automatiquement.',
     ],
