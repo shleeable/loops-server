df411771632de00f598f83a5505ade56dc76a221
Daniel Supernault <danielsupernault@gmail.com>
2025-09-10 04:09:58 -0600

    Increase default session lifetime

--- config/session.php
+++ config/session.php
@@ -32,7 +32,7 @@ return [
     |
     */
 
-    'lifetime' => env('SESSION_LIFETIME', 120),
+    'lifetime' => env('SESSION_LIFETIME', 86400),
 
     'expire_on_close' => env('SESSION_EXPIRE_ON_CLOSE', false),
 
