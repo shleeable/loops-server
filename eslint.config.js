e8c21214ae1701e7dcf58a2ec0b18c455c56ba7a
Shlee <github@shl.ee>
2025-09-13 15:28:34 +0930

    Update eslint.config.js - including typescript-eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,14 @@
 import js from '@eslint/js'
 import vue from 'eslint-plugin-vue'
 import globals from "globals";
+import tseslint from 'typescript-eslint'
 
 export default [
   js.configs.recommended,
   ...vue.configs['flat/essential'],
+  ...tseslint.configs.recommended,
   {
-    files: ['resources/**/*.{js,vue}'],
+    files: ['resources/**/*.{js,vue,ts}'],
     languageOptions: {
       ecmaVersion: 2022,
       sourceType: 'module',
@@ -22,12 +24,12 @@ export default [
     rules: {
       'vue/multi-word-component-names': 'off',
       'no-console': 'off',
-      'vue/multi-word-component-names': 'off',
       'vue/no-v-html': 'warn',
       'vue/require-default-prop': 'off',
       'vue/require-explicit-emits': 'error',
       'no-debugger': 'off',
-      'no-unused-vars': 'warn'
+      'no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn'
     }
   }
 ]
