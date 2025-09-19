22e9be60da87f0609b9b9e47aa2dbe20b253ada4
Daniel Supernault <danielsupernault@gmail.com>
2025-09-18 22:56:55 -0600

    Add error views

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,31 @@
+@php($title = \App\Services\FrontendService::getAppName())
+@php($desc = \App\Services\FrontendService::getAppDescription())
+@php($favicon = \App\Services\FrontendService::getAppFavicon())
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>{{ $title }}</title>
+    <link rel="shortcut icon" type="image/png" href="{{$favicon}}"/>
+    <meta name="description" content="{{ $desc }}">
+    <meta property="og:title" content="{{ $title }}" />
+    <meta property="og:description" content="{{ $desc }}" />
+    <meta property="og:type" content="website" />
+    <meta name="twitter:title" content="{{ $title }}" />
+    <meta name="twitter:description" content="{{ $desc }}" />
+    @vite(['resources/js/app.js']){!!App\Services\FrontendService::getCustomCss();!!}
+
+    <script type="text/javascript">
+    window.appConfig = {!! \App\Services\FrontendService::getAppData() !!};
+    window._navi = {!!App\Services\PageService::getActiveSideLinks();!!};
+    {!!App\Services\FrontendService::getCaptchaData();!!}
+    </script>
+</head>
+
+<body class="bg-white dark:bg-slate-950">
+    <main id="app">
+        <router-view></router-view>
+    </main>
+</body>
+</html>
