3b781860cb8210a6c1ee4615251ba76105b46491
Daniel Supernault <danielsupernault@gmail.com>
2025-10-07 23:23:39 -0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,12 @@
 @php($title = \App\Services\FrontendService::getAppName())
 @php($desc = \App\Services\FrontendService::getAppDescription())
-@php($favicon = \App\Services\FrontendService::getAppFavicon())
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>{{ $title }}</title>
-    <link rel="shortcut icon" type="image/png" href="{{$favicon}}"/>
+    <link rel="shortcut icon" type="image/png" href="{{ url('/favicon.png') }}"/>
     <meta name="description" content="{{ $desc }}">
     <meta property="og:title" content="{{ $title }}" />
     <meta property="og:description" content="{{ $desc }}" />
