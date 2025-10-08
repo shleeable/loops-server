728153d456446bb4f8aba83ebe4b1863993570f1
Daniel Supernault <danielsupernault@gmail.com>
2025-10-07 19:37:33 -0600

    Create ConfigService.php

---
+++ app/Services/ConfigService.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\AdminSetting;
+use Illuminate\Support\Facades\Cache;
+
+class ConfigService
+{
+    const CACHE_KEY = 'loops:services:config:';
+
+    public function domain()
+    {
+        return parse_url(config('app.url'), PHP_URL_HOST);
+    }
+
+    public function federation($flush = false)
+    {
+        $key = self::CACHE_KEY.'federation';
+
+        if ($flush) {
+            Cache::forget($key);
+        }
+
+        return Cache::rememberForever($key, function () {
+            $config = AdminSetting::where('key', 'federation.enableFederation')->first();
+            if (! $config) {
+                app(SettingsFileService::class)->generatePublicConfig();
+
+                return false;
+            }
+
+            return (bool) $config->value;
+        });
+    }
+
+    public function federationMode($flush = false)
+    {
+        $key = self::CACHE_KEY.'federationMode';
+
+        if ($flush) {
+            Cache::forget($key);
+        }
+
+        return Cache::rememberForever($key, function () {
+            $config = AdminSetting::where('key', 'federation.federationMode')->first();
+            if (! $config) {
+                app(SettingsFileService::class)->generatePublicConfig();
+
+                return 'lockdown';
+            }
+
+            return $config->value;
+        });
+    }
+
+    public function federationAllowedServers($flush = false)
+    {
+        $key = self::CACHE_KEY.'federation:lockdown:allowed';
+
+        if ($flush) {
+            Cache::forget($key);
+        }
+
+        return Cache::rememberForever($key, function () {
+            $config = AdminSetting::where('key', 'federation.allowedInstances')->first();
+            if (! $config) {
+                app(SettingsFileService::class)->generatePublicConfig();
+
+                return [];
+            }
+
+            return $config->value;
+        });
+    }
+
+    public function federationAuthorizedFetch($flush = false)
+    {
+        $key = self::CACHE_KEY.'federation:lockdown:authorizedFetch';
+
+        if ($flush) {
+            Cache::forget($key);
+        }
+
+        return Cache::rememberForever($key, function () {
+            $config = AdminSetting::where('key', 'federation.authorizedFetch')->first();
+            if (! $config) {
+                app(SettingsFileService::class)->generatePublicConfig();
+
+                return false;
+            }
+
+            return (bool) $config->value;
+        });
+    }
+}
