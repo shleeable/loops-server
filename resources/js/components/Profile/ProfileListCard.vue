b60d9c0fc342830d1d1b10afcc1d9a4e86ee2a91
Daniel Supernault <danielsupernault@gmail.com>
2025-11-22 03:31:46 -0700

    Update ProfileListCard, line-clamp name and username

--- resources/js/components/Profile/ProfileListCard.vue
+++ resources/js/components/Profile/ProfileListCard.vue
@@ -26,10 +26,12 @@ const { account } = props;
     <div class="flex items-center space-x-3">
         <Avatar :src="account.avatar" :width="40" />
         <div>
-            <div class="font-medium text-[15px] dark:text-slate-200">
+            <div
+                class="font-medium text-[15px] dark:text-slate-200 line-clamp-1"
+            >
                 {{ account.name }}
             </div>
-            <div class="font-semibold text-gray-500 text-[12px]">
+            <div class="font-semibold text-gray-500 text-[12px] line-clamp-1">
                 @{{ account.username }}
             </div>
         </div>
