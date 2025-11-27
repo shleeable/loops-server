3e6b601e47205fd77cfbe95a72cf2ac1bbfb4182
Daniel Supernault <danielsupernault@gmail.com>
2025-11-26 18:03:33 -0700

    Create PlaylistPolicy.php

---
+++ app/Policies/PlaylistPolicy.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Playlist;
+use App\Models\User;
+
+class PlaylistPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Playlist $playlist): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        $profile = $user->profile;
+
+        if ($profile->followers < 100 || $profile->video_count < 20) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Playlist $playlist): bool
+    {
+        return (int) $user->profile_id === (int) $playlist->profile_id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Playlist $playlist): bool
+    {
+        return $user->profile_id === $playlist->profile_id;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Playlist $playlist): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Playlist $playlist): bool
+    {
+        return false;
+    }
+}
