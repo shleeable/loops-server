6c6d219ce186afa6f807f297b8d01b647493d3f6
Shlee <github@shl.ee>
2025-11-29 16:05:58 +1030

    Additional og: metadata - Update video.blade.php

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -18,6 +18,12 @@ $authorName = data_get($videoData, 'account.name', '');
 $authorUsername = data_get($videoData, 'account.username', '');
 $authorAvatar = data_get($videoData, 'account.avatar', url('/storage/avatars/default.jpg'));
 
+$videoCdnUrl = data_get($videoData, 'src_url', '');
+videoWidth = data_get($videoData, 'media.width', '');
+videoHeight = data_get($videoData, 'media.height', '');
+videoDuration =  '';
+$videoType ='video/mp4';
+
 if ($videoData) {
     if (!empty($videoData['captionText'])) {
         $videoDesc = $videoData['captionText'];
@@ -40,17 +46,22 @@ if ($videoData) {
 
     <meta property="og:title" content="{{ $videoTitle }}" />
     <meta property="og:description" content="{{ $videoDesc }}" />
-    <meta property="og:type" content="video.other" />
+    <meta property="og:type" content="video" />
     <meta property="og:url" content="{{ $videoUrl }}" />
     <meta property="og:image" content="{{ $videoThumbnail }}" />
+    <meta property="og:video" content="{{ $videoCdnUrl }}" />
+    <meta property="og:video:width" content="{{ $videoWidth }}">
+    <meta property="og:video:height" content="{{ $videoHeight }}">
+    <meta property="og:video:duration" content="{{ $videoDuration }}">
+    <meta property="og:video:type" content="{{ $videoType }}" />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:logo" content="{{ url('/nav-logo.png') }}" />
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:title" content="{{ $videoTitle }}" />
     <meta name="twitter:description" content="{{ $videoDesc }}" />
     <meta name="twitter:image" content="{{ $videoThumbnail }}" />
-    <meta property="og:locale" content="en_US" />
-    <meta property="og:logo" content="{{ url('/nav-logo.png') }}" />
-    <meta property="video:release_date" content="{{ $videoData['created_at'] ?? '' }}" />
 
+    <meta property="video:release_date" content="{{ $videoData['created_at'] ?? '' }}" />
     <meta property="article:author" content="{{ url('/@' . $authorUsername) }}" />
 
     @vite(['resources/js/app.js'])
