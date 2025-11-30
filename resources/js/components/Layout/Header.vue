39be8143c1d024f92c840faf27d77bff256bce35
Daniel Supernault <danielsupernault@gmail.com>
2025-11-29 21:10:32 -0700

    Update header fix stroke width

--- resources/js/components/Layout/Header.vue
+++ resources/js/components/Layout/Header.vue
@@ -211,7 +211,7 @@
                                     <path
                                         stroke-linecap="round"
                                         stroke-linejoin="round"
-                                        stroke-width="2"
+                                        stroke-width="1.1"
                                         d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
                                     />
                                 </svg>
@@ -452,11 +452,9 @@ const closeMobileSearch = () => {
     showDropdown.value = false
 }
 
-// Load recent searches on component mount
 onMounted(() => {
     searchStore.loadRecentSearches()
 
-    // Close dropdowns when clicking outside
     document.addEventListener('mouseup', (e) => {
         const searchContainer = document.querySelector('.search-container')
         const popupMenu = document.getElementById('PopupMenu')
@@ -469,14 +467,12 @@ onMounted(() => {
             showMenu.value = false
         }
 
-        // Close mobile search when clicking outside
         if (showMobileSearch.value && !e.target.closest('.absolute.top-full')) {
             closeMobileSearch()
         }
     })
 })
 
-// Debounced search function
 const handleSearch = debounce(() => {
     searchStore.searchQuery(searchQuery.value)
 }, 300)
