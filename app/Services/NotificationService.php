37dc643d3453c6f39a73e4217dd95b443879dd77
Daniel Supernault <danielsupernault@gmail.com>
2025-12-29 00:51:46 -0700

    Update NotificationService, improve counter cache invalidation

--- app/Services/NotificationService.php
+++ app/Services/NotificationService.php
@@ -58,12 +58,15 @@ class NotificationService
 
     public static function newVideoLike($uid, $vid, $pid)
     {
-        return Notification::updateOrCreate([
+        $res = Notification::updateOrCreate([
             'type' => Notification::VIDEO_LIKE,
             'user_id' => $uid,
             'video_id' => $vid,
             'profile_id' => $pid,
         ]);
+        self::clearUnreadCount($uid);
+
+        return $res;
     }
 
     public static function deleteVideoLike($uid, $vid, $pid)
@@ -142,11 +145,15 @@ class NotificationService
 
     public static function newFollower($uid, $pid)
     {
-        return Notification::updateOrCreate([
+        $res = Notification::updateOrCreate([
             'type' => Notification::NEW_FOLLOWER,
             'user_id' => $uid,
             'profile_id' => $pid,
         ]);
+
+        self::clearUnreadCount($uid);
+
+        return $res;
     }
 
     public static function unFollow($uid, $pid)
@@ -165,13 +172,16 @@ class NotificationService
 
     public static function newVideoComment($pid, $uid, $vid, $cid)
     {
-        return Notification::updateOrCreate([
+        $res = Notification::updateOrCreate([
             'type' => Notification::NEW_VIDCOMMENT,
             'user_id' => $uid,
             'profile_id' => $pid,
             'video_id' => $vid,
             'comment_id' => $cid,
         ]);
+        self::clearUnreadCount($uid);
+
+        return $res;
     }
 
     public static function newVideoDuet($pid, $uid, $vid)
@@ -249,12 +259,15 @@ class NotificationService
 
     public static function newVideoShare($uid, $vid, $pid)
     {
-        return Notification::updateOrCreate([
+        $res = Notification::updateOrCreate([
             'type' => Notification::VIDEO_SHARE,
             'user_id' => $uid,
             'video_id' => $vid,
             'profile_id' => $pid,
         ]);
+        self::clearUnreadCount($uid);
+
+        return $res;
     }
 
     public static function deleteVideoShare($uid, $vid, $pid)
@@ -274,13 +287,16 @@ class NotificationService
 
     public static function newVideoCommentShare($uid, $cid, $vid, $pid)
     {
-        return Notification::updateOrCreate([
+        $res = Notification::updateOrCreate([
             'type' => Notification::VIDEO_COMMENT_SHARE,
             'user_id' => $uid,
             'comment_id' => $cid,
             'video_id' => $vid,
             'profile_id' => $pid,
         ]);
+        self::clearUnreadCount($uid);
+
+        return $res;
     }
 
     public static function deleteVideoCommentShare($uid, $cid, $vid, $pid)
@@ -301,13 +317,16 @@ class NotificationService
 
     public static function newVideoReplyShare($uid, $cid, $vid, $pid)
     {
-        return Notification::updateOrCreate([
+        $res = Notification::updateOrCreate([
             'type' => Notification::VIDEO_REPLY_SHARE,
             'user_id' => $uid,
             'comment_reply_id' => $cid,
             'video_id' => $vid,
             'profile_id' => $pid,
         ]);
+        self::clearUnreadCount($uid);
+
+        return $res;
     }
 
     public static function deleteVideoReplyShare($uid, $cid, $vid, $pid)
