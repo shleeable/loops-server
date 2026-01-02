06fbdc2f02be8850ac9d719799c7ab82a0b821df
Daniel Supernault <danielsupernault@gmail.com>
2026-01-01 18:36:00 -0700

    Update DataTable.vue

--- resources/js/components/DataTable.vue
+++ resources/js/components/DataTable.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="bg-white dark:bg-gray-800 shadow-sm rounded-lg border border-gray-200 dark:border-gray-700"
+        class="bg-white dark:bg-gray-800 shadow-sm rounded-lg border border-gray-200 dark:border-gray-700 mb-50 lg:mb-20"
     >
         <div class="px-6 py-4 border-b border-gray-200 dark:border-gray-700">
             <div class="flex items-center justify-between">
