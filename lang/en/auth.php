23da0d8763da96c5124164e0e48fa4028a81b3c0
Daniel Supernault <danielsupernault@gmail.com>
2026-01-08 03:36:44 -0700

    Update AuthModal, add legal to register

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -1,9 +1,13 @@
 <?php
 
 return [
+    'byCreatingANewAccount' => 'By creating a new account, you agree to the',
+    'communityGuidelines' => 'Community Guidelines',
     'failed' => 'These credentials do not match our records.',
     'password' => 'The provided password is incorrect.',
+    'privacyPolicy' => 'Privacy Policy',
     'registrationsAreCurrentlyClosed' => 'Registrations are currently closed',
+    'termsOfService' => 'Terms of Service',
     'thisServerIsntAccepting' => 'This server isn’t accepting new signups right now. Please check back later or choose another Loops server.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 ];
