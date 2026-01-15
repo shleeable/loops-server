f026521b923362ebeeb61ce1b8fe42ba30f824fc
Daniel Supernault <danielsupernault@gmail.com>
2026-01-15 00:47:54 -0700

    Update ActivityPubContext

--- app/Support/ActivityPubContext.php
+++ app/Support/ActivityPubContext.php
@@ -36,6 +36,9 @@ class ActivityPubContext
             'https://www.w3.org/ns/activitystreams',
             'https://w3id.org/security/v1',
             [
+                'quote' => 'https://w3id.org/fep/044f#quote',
+                'quoteUri' => 'http://fedibird.com/ns#quoteUri',
+                '_misskey_quote' => 'https://misskey-hub.net/ns#_misskey_quote',
                 'quoteAuthorization' => [
                     '@id' => 'https://w3id.org/fep/044f#quoteAuthorization',
                     '@type' => '@id',
