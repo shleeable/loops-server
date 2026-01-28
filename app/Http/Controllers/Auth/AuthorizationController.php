c2d08ff28db77e33a840576ce3748dcfcf5dfa7b
Daniel Supernault <danielsupernault@gmail.com>
2026-01-28 00:51:09 -0700

    Update AuthorizationController

--- app/Http/Controllers/Auth/AuthorizationController.php
+++ app/Http/Controllers/Auth/AuthorizationController.php
@@ -2,16 +2,37 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Contracts\Auth\StatefulGuard;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Laravel\Passport\Bridge\User;
+use Laravel\Passport\Client;
+use Laravel\Passport\ClientRepository;
 use Laravel\Passport\Contracts\AuthorizationViewResponse;
-use Laravel\Passport\Http\Controllers\AuthorizationController as PassportAuthorizationController;
+use Laravel\Passport\Http\Controllers\AuthorizationController as PassportController;
+use Laravel\Passport\Http\Controllers\ConvertsPsrResponses;
+use Laravel\Passport\Http\Controllers\HandlesOAuthErrors;
+use League\OAuth2\Server\AuthorizationServer;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Symfony\Component\HttpFoundation\Response;
 
-class AuthorizationController extends PassportAuthorizationController
+class AuthorizationController extends PassportController
 {
+    use ConvertsPsrResponses, HandlesOAuthErrors;
+
+    /**
+     * Create a new controller instance.
+     */
+    public function __construct(
+        protected AuthorizationServer $server,
+        protected StatefulGuard $guard,
+        protected ClientRepository $clients,
+    ) {}
+
+    /**
+     * Authorize a client to access the user's account.
+     */
     public function authorize(
         ServerRequestInterface $psrRequest,
         Request $request,
