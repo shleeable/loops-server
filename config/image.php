38b3a7e5d84e3490453cc0abecc138477970c655
Daniel Supernault <danielsupernault@gmail.com>
2026-02-10 04:25:46 -0700

    Lint

--- config/image.php
+++ config/image.php
@@ -2,14 +2,15 @@
 
 use Intervention\Image\Drivers\Gd\Driver as GdDriver;
 use Intervention\Image\Drivers\Imagick\Driver as ImagickDriver;
-//use Intervention\Image\Drivers\Vips\Driver as VipsDriver;
+
+// use Intervention\Image\Drivers\Vips\Driver as VipsDriver;
 
 $driver = env('IMAGE_DRIVER', 'gd');
 
 $driverClass = match (strtolower((string) $driver)) {
     'gd' => GdDriver::class,
     'imagick' => ImagickDriver::class,
-    //'vips' => VipsDriver::class,
+    // 'vips' => VipsDriver::class,
     default => class_exists($driver) ? $driver : GdDriver::class,
 };
 
