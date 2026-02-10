88631615d49d95bcddc91a31bfaa0ffd222d0c98
Daniel Supernault <danielsupernault@gmail.com>
2026-02-10 04:08:12 -0700

    Update image config

--- config/image.php
+++ config/image.php
@@ -1,41 +1,32 @@
 <?php
 
+use Intervention\Image\Drivers\Gd\Driver as GdDriver;
+use Intervention\Image\Drivers\Imagick\Driver as ImagickDriver;
+//use Intervention\Image\Drivers\Vips\Driver as VipsDriver;
+
+$driver = env('IMAGE_DRIVER', 'gd');
+
+$driverClass = match (strtolower((string) $driver)) {
+    'gd' => GdDriver::class,
+    'imagick' => ImagickDriver::class,
+    //'vips' => VipsDriver::class,
+    default => class_exists($driver) ? $driver : GdDriver::class,
+};
+
 return [
 
     /*
     |--------------------------------------------------------------------------
     | Image Driver
     |--------------------------------------------------------------------------
-    |
-    | Intervention Image supports “GD Library” and “Imagick” to process images
-    | internally. Depending on your PHP setup, you can choose one of them.
-    |
-    | Included options:
-    |   - \Intervention\Image\Drivers\Gd\Driver::class
-    |   - \Intervention\Image\Drivers\Imagick\Driver::class
-    |   - \Intervention\Image\Drivers\Vips\Driver::class
-    |
     */
 
-    'driver' => \Intervention\Image\Drivers\Gd\Driver::class,
+    'driver' => $driverClass,
 
     /*
     |--------------------------------------------------------------------------
     | Configuration Options
     |--------------------------------------------------------------------------
-    |
-    | These options control the behavior of Intervention Image.
-    |
-    | - "autoOrientation" controls whether an imported image should be
-    |    automatically rotated according to any existing Exif data.
-    |
-    | - "decodeAnimation" decides whether a possibly animated image is
-    |    decoded as such or whether the animation is discarded.
-    |
-    | - "blendingColor" Defines the default blending color.
-    |
-    | - "strip" controls if meta data like exif tags should be removed when
-    |    encoding images.
     */
 
     'options' => [
