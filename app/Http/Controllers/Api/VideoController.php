74831b6d1ca6dbecaeab13267cbe77dadaddfc4f
Shlee <github@shl.ee>
2026-08-11 15:52:42 +0930

    Update VideoController.php (#1334)

--- app/Http/Controllers/Api/VideoController.php
+++ app/Http/Controllers/Api/VideoController.php
@@ -298,10 +298,10 @@ class VideoController extends Controller
             ]);
         }
         $video->caption = $updatedCaption;
-        $video->can_download = $request->has('can_download') ? $request->boolean('can_download') : false;
+        $video->can_download = $request->has('can_download') ? $request->boolean('can_download') : $video->can_download;
         $video->can_embed = $canEmbed && $request->has('can_embed') ? $request->boolean('can_embed') : $video->can_embed;
-        $video->comment_state = $request->has('can_comment') ? ($request->boolean('can_comment') ? 4 : 0) : 0;
-        $video->is_pinned = $request->has('is_pinned') ? $request->boolean('is_pinned') : 0;
+        $video->comment_state = $request->has('can_comment') ? ($request->boolean('can_comment') ? 4 : 0) : $video->comment_state;
+        $video->is_pinned = $request->has('is_pinned') ? $request->boolean('is_pinned') : $video->is_pinned;
         $video->pinned_order = $request->has('is_pinned') ? Video::whereStatus(2)->whereProfileId($pid)->whereIsPinned(true)->count() + 1 : 0;
         $video->alt_text = $request->filled('alt_text') ? app(SanitizeService::class)->cleanPlainText($request->alt_text) : null;
         $video->can_duet = $request->has('can_duet') ? $request->boolean('can_duet') : $video->can_duet;
