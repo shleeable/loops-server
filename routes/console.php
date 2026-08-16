d300574c7aafb42837dc937535088ba88b4810f6
Daniel Supernault <danielsupernault@gmail.com>
2026-08-16 03:39:10 -0600

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -3,27 +3,27 @@
 use App\Console\Commands\SnapshotUsage;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command(SnapshotUsage::class)->dailyAt('00:05')->timezone('UTC')->onOneServer();
-Schedule::command('app:expire-user-register-verifications')->everyFiveMinutes()->onOneServer();
-Schedule::command('horizon:snapshot')->hourly()->onOneServer();
-Schedule::command('instances:update-stats')->daily()->at('04:20')->onOneServer();
-Schedule::command('feed:update-interests')->hourly()->onOneServer();
-Schedule::command('app:system-message-seeder')->daily()->onOneServer();
-Schedule::command('app:system-message-garbage-collector')->daily()->onOneServer();
-Schedule::command('passport:purge --revoked')->daily()->onOneServer();
+Schedule::command(SnapshotUsage::class)->dailyAt('00:05')->timezone('UTC')->runInBackground()->onOneServer();
+Schedule::command('app:expire-user-register-verifications')->everyFiveMinutes()->runInBackground()->onOneServer();
+Schedule::command('horizon:snapshot')->hourly()->runInBackground()->onOneServer();
+Schedule::command('instances:update-stats')->daily()->at('04:20')->runInBackground()->onOneServer();
+Schedule::command('feed:update-interests')->hourly()->runInBackground()->onOneServer();
+Schedule::command('app:system-message-seeder')->daily()->runInBackground()->onOneServer();
+Schedule::command('app:system-message-garbage-collector')->daily()->runInBackground()->onOneServer();
+Schedule::command('passport:purge --revoked')->daily()->runInBackground()->onOneServer();
 Schedule::command('version:check --force')->twiceDaily(9, 17)->withoutOverlapping()->runInBackground()->onOneServer();
-Schedule::command('app:instance-stats-collector-command')->everySixHours(20)->withoutOverlapping()->onOneServer();
-Schedule::command('logs:clean-admin-security --days=14')->dailyAt('03:00')->onOneServer();
-Schedule::command('curated:expire-stale')->hourly()->onOneServer();
-Schedule::command('admin:check-activity')->everySixHours(15)->onOneServer();
-Schedule::command('app:purge-old-activities')->daily()->at('16:20')->onOneServer();
-Schedule::command('loops:publish-scheduled')->everyFifteenMinutes()->withoutOverlapping(5)->runInBackground()->onOneServer();
+Schedule::command('app:instance-stats-collector-command')->everySixHours(20)->withoutOverlapping()->runInBackground()->onOneServer();
+Schedule::command('logs:clean-admin-security --days=14')->dailyAt('03:00')->runInBackground()->onOneServer();
+Schedule::command('curated:expire-stale')->hourly()->runInBackground()->onOneServer();
+Schedule::command('admin:check-activity')->everySixHours(15)->runInBackground()->onOneServer();
+Schedule::command('app:purge-old-activities')->daily()->at('16:20')->runInBackground()->onOneServer();
+Schedule::command('loops:publish-scheduled')->everyTenMinutes()->withoutOverlapping(5)->runInBackground()->onOneServer();
 
 if (config('loops.admin_dashboard.autoUpdate')) {
-    Schedule::command('admin:refresh-dashboard-30d')->everyThirtyMinutes()->onOneServer();
+    Schedule::command('admin:refresh-dashboard-30d')->everyThirtyMinutes()->runInBackground()->onOneServer();
 }
 
 if (config('loops.backups.enabled')) {
-    Schedule::command('backup:clean')->daily()->at('01:00')->onOneServer();
-    Schedule::command('backup:run --only-db')->everySixHours()->onOneServer();
+    Schedule::command('backup:clean')->daily()->at('01:00')->runInBackground()->onOneServer();
+    Schedule::command('backup:run --only-db')->everySixHours()->runInBackground()->onOneServer();
 }
